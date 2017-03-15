e72ee0318132152930f581433a3f6268e2992513
Andras Kantor <and.kantor@gmail.com>
2017-03-15 18:51:44 +0100

    Basic home page

--- src/main/java/andkantor/f1betting/controller/HomeController.java
+++ src/main/java/andkantor/f1betting/controller/HomeController.java
@@ -1,13 +1,42 @@
 package andkantor.f1betting.controller;
 
+import andkantor.f1betting.entity.Season;
+import andkantor.f1betting.model.setting.ConfigurationManager;
+import andkantor.f1betting.repository.RaceRepository;
+import andkantor.f1betting.repository.SeasonRepository;
+import andkantor.f1betting.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    SeasonRepository seasonRepository;
+
+    @Autowired
+    ConfigurationManager configurationManager;
+
+    @Autowired
+    RaceRepository raceRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        Long activeSeason = configurationManager.getConfiguration().getActiveSeason();
+
+        if (activeSeason != 0) {
+            Season season = seasonRepository.findOne(activeSeason);
+            model.addAttribute("season", season);
+            model.addAttribute("races", raceRepository.findBySeason(season));
+        }
+
+        model.addAttribute("users", userRepository.findByEnabledAndUsernameNot(true, "admin"));
+
         return "home";
     }
 
--- src/main/java/andkantor/f1betting/repository/UserRepository.java
+++ src/main/java/andkantor/f1betting/repository/UserRepository.java
@@ -3,8 +3,12 @@ package andkantor.f1betting.repository;
 import andkantor.f1betting.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByUsername(String username);
+
+    List<User> findByEnabledAndUsernameNot(boolean enabled, String username);
 
 }
